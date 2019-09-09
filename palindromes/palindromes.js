7e06aeaf068bfc69ffb006df64c08e4034118bb4
Shing Tze Woo <shingtzewoo@gmail.com>
2019-09-09 15:11:13 -0700

    solution added

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function() {
+let palindromes = function(stringg) {
+    let arr = stringg.toLowerCase().split('');
+    let filteredarr = arr.filter((value) => value.match(/^[a-zA-Z]/));
+    let arr4reversal = filteredarr.map((value) => value); //used as the value to reverse because we don't want the filteredarr to be reversed
+    let reversedarr = arr4reversal.reverse();
 
+    //joining the arrays into strings for comparison
+    let reversedString = reversedarr.join('');
+    let originalString = filteredarr.join('');
+
+    //comparsion
+    return reversedString === originalString ? true : false;
 }
 
 module.exports = palindromes
