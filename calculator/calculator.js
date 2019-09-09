9cf4a0ab2d97a2a9d9c3e31872ad542717f3afca
Shing Tze Woo <shingtzewoo@gmail.com>
2019-09-08 19:13:13 -0700

    reduce method used for factorial function

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -20,12 +20,33 @@ function multiply (arr) {
 	return result;
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(x) {
+	let result = 1;
+	let counter = 1;
+	if (x === 0 || x === 1) {
+		return result;
+	} else {
+		while (counter < x) {
+			result *= (x*counter)
+			counter++;
+		}
+	}
+	return result;
+}
+
+function factorial(x) {
+	let arr = [];
+	for (let i = 1; i <= x; i++) {
+		arr.push(i);
+	}
+	let result = arr.reduce(function(total, current) {
+		return total *= current;
+	}, 1);
+	return result;
 }
 
 module.exports = {
