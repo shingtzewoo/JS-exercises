02c044a44a552f638486cf03d9eb274c72bd99e9
Shing Tze Woo <shingtzewoo@gmail.com>
2019-09-24 10:23:17 -0700

    solution added using for loop and array

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let a = 1;
+    let b = 1;
+    let arr = [];
+    (typeof(member) == 'string') ? member = parseInt(member, 10) : member;
+    if (member <= 0) {
+        return "OOPS"
+    }
+    for (let count = 1; count <= member;) {
+        arr[count] = a;
+        count++;
+        arr[count] = b;
+        count++;
+        a = a + b;
+        b = b + a;
+    }
+    return arr[member];
 }
 
 module.exports = fibonacci
