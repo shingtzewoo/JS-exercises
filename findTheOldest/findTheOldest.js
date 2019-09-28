a7cdf7347182d0ec3fadde62a980ed478cfee0e0
Shing Tze Woo <shingtzewoo@gmail.com>
2019-09-28 14:10:03 -0700

    sort method and return first item in sorted array

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    let d = new Date();
+    let result = people
+        .sort(function(a, b) {
+            if (!a.yearOfDeath) {
+                a.yearOfDeath = d.getFullYear();
+            }
+            if (!b.yearOfDeath) {
+                b.yearOfDeath = d.getFullYear();
+            }
+            const first = a.yearOfDeath - a.yearOfBirth;
+            const next = b.yearOfDeath - b.yearOfBirth;
+            return first > next ? -1 : 1;
+        })
+    return result[0];
 }
-
 module.exports = findTheOldest
